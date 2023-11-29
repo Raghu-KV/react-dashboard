b95f7dc00f57d7da088575a7a9343c8e62699e4b
Raghu-KV <raghunandanv19@gmail.com>
2023-11-29 19:07:58 +0530

    sd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import SideBar from "../components/SideBar";
 import NavBar from "../components/NavBar";
+import Wiget from "../components/Wiget";
 function Home() {
   return (
     <div className="flex">
       <SideBar />
       <div className="basis-5/6">
         <NavBar />
-        Container
+        <div className="flex gap-7 px-6 py-4">
+          <Wiget />
+          <Wiget />
+          <Wiget />
+          <Wiget />
+        </div>
       </div>
     </div>
   );
