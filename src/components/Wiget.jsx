ea5811bd1640146f0826868ec3b50d6039019a83
Raghu-KV <raghunandanv19@gmail.com>
2023-11-29 22:16:15 +0530

    changed

--- src/components/Wiget.jsx
+++ src/components/Wiget.jsx
@@ -1,14 +1,23 @@
 import { MdArrowForward } from "react-icons/md";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
-import { MdOutlinePerson } from "react-icons/md";
-function Wiget() {
+import { MdCurrencyRupee } from "react-icons/md";
+function Wiget({ data }) {
   return (
     <div className="flex flex-1 justify-between border-2 font-poppins p-3 rounded-xl bg-white duration-200">
       <div className="flex flex-col gap-5">
-        <p className="font-bold text-gray-500">USERS</p>
-        <h3 className="text-5xl">100</h3>
+        <p className="font-bold text-gray-500">{data.title}</p>
+        <h3 className="text-5xl">
+          {data.isCurrency ? (
+            <>
+              <MdCurrencyRupee size={30} className="inline-block" />
+              {data.count}
+            </>
+          ) : (
+            data.count
+          )}
+        </h3>
         <p className="border-b-2 border-gray-500 text-gray-500">
-          See All Users
+          {data.linkContent}
           <MdArrowForward size={20} className="ml-1 inline-block" />
         </p>
       </div>
@@ -17,8 +26,10 @@ function Wiget() {
           <MdKeyboardDoubleArrowUp size={25} />
           <p className="font-semibold ">10%</p>
         </div>
-        <div className="bg-yellow-200 p-1 rounded-xl">
-          <MdOutlinePerson size={30} className="text-yellow-600" />
+        <div
+          className={`bg-${data.accentColor}-200 p-1 rounded-xl text-${data.accentColor}-600`}
+        >
+          {data.icon}
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,55 @@ import Wiget from "../components/Wiget";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 import { MdArrowForward } from "react-icons/md";
 import { MdOutlinePerson } from "react-icons/md";
+import { MdShoppingCart } from "react-icons/md";
+import { MdCurrencyRupee } from "react-icons/md";
+import { MdAccountBalanceWallet } from "react-icons/md";
+
 function Home() {
+  const wigetData = [
+    {
+      title: "USERS",
+      count: 101,
+      isCurrency: false,
+      linkContent: "See all users",
+      accentColor: "yellow",
+      icon: <MdOutlinePerson size={25} />,
+    },
+    {
+      title: "ORDERS",
+      count: 12,
+      isCurrency: false,
+      linkContent: "View all orders",
+      accentColor: "blue",
+      icon: <MdShoppingCart size={25} />,
+    },
+    {
+      title: "EARNINGS",
+      count: 20000,
+      isCurrency: true,
+      linkContent: "See details",
+      accentColor: "green",
+      icon: <MdCurrencyRupee size={25} />,
+    },
+    {
+      title: "BALANCE",
+      count: 10000,
+      isCurrency: true,
+      linkContent: "See details",
+      accentColor: "violet",
+      icon: <MdAccountBalanceWallet size={25} />,
+    },
+  ];
+
   return (
     <div className="flex">
       <SideBar />
       <div className="basis-5/6">
         <NavBar />
         <div className="flex gap-7 px-6 py-4">
-          <Wiget />
-          <Wiget />
-          <Wiget />
-          <Wiget />
+          {wigetData.map((data) => (
+            <Wiget data={data} key={data.title} />
+          ))}
         </div>
       </div>
     </div>
